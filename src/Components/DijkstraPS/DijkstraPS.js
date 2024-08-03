67da054f4319e89ad32989440da65894d395d5b9
NavikaReddy <navikareddy12@gmail.com>
2024-08-03 15:55:34 +0530

    dijkstra ps

---
+++ src/Components/DijkstraPS/DijkstraPS.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function DijkstraPS() {
+  return (
+    <div>
+        <h1>Dijkstra Algorithm</h1>
+        <h3>Problem Statement</h3>
+        <p>There are n cities numbered from 1 to n. Given the array edges where edges[i] = [fromi, toi, weighti] represents a bidirectional and weighted edge between cities fromi and toi.
+          Return the least distance path from source city to destination city.
+        Notice that the distance of a path connecting cities i and j is equal to the sum of the edges' weights along that path.</p>
+    </div>
+  )
+}
+
+export default DijkstraPS
